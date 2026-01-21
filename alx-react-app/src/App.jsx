6e70de2d0fdd9e5e3ea40eb3476b4d63b931ab77
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2026-01-22 00:36:52 +0200

    my first react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,10 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App(WelcomeMessage) {
   const [count, setCount] = useState(0)
 
   return (
