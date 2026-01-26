492af672129104125e438e6e598b204d7f229a1d
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2026-01-26 23:21:19 +0200

    Resolve merge conflict

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,7 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import './App.css'
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
